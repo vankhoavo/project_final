e64608f8f275123d67428faa4009d4f3a72c5c79
VoVanKhoa <vovankhoa2001@hotmail.com>
2023-09-20 12:52:07 +0700

    improve1

--- app/Http/Controllers/FacebookAuthController.php
+++ app/Http/Controllers/FacebookAuthController.php
@@ -4,14 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Facades\Socialite as FacadesSocialite;
 use Laravel\Socialite\Facades\Socialite;
 
 class FacebookAuthController extends Controller
 {
     public function redirectToFacebook()
     {
-        return FacadesSocialite::driver('facebook')->redirect();
+        return Socialite::driver('facebook')->redirect();
     }
 
     public function handleFacebookCallback()
